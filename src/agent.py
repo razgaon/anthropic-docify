f89bac35d3e85c7f53632c91b169c1f29f43c578
Raz Gaon <razgaon@mit.edu>
2023-07-29 15:26:27 -0700

    Added full e2e

--- src/agent.py
+++ src/agent.py
@@ -8,14 +8,20 @@ load_dotenv()
 from templates import IMPROVE_PAGE_TEMPLATE, CRITIQUE_PAGE_TEMPLATE, FEEDBACK_IMPROVE_PAGE_TEMPLATE
 chat = ChatAnthropic(model='claude-2')
 
-def get_improved_page(reference_page: str, context: str) -> str:
+def get_improved_page(reference_page: str, context: str, reference_page_name: str) -> str:
     # Step 1: Given context and a reference page, generate an improved page
     improve_page_chain = LLMChain(llm=chat, prompt=PromptTemplate.from_template(IMPROVE_PAGE_TEMPLATE))
     improved_page = improve_page_chain.run(context=context, reference_page=reference_page)
-    return improved_page
+    
+    with(open(f'./output/v1/{reference_page_name}.md', 'w')) as f:
+        f.write(improved_page)
+    
     # Step 2: Given the improved page, critique it and provide feedback
     critique_page_chain = LLMChain(llm=chat, prompt=PromptTemplate.from_template(CRITIQUE_PAGE_TEMPLATE))
     critique = critique_page_chain.run(inputs={"context": context, "reference_page": reference_page, improved_page: improved_page})
+    
+    with(open(f'./output/v2/{reference_page_name}.md', 'w')) as f:
+        f.write(critique)
 
     # Step 3: Given an improved page and critique, generate a new improved page
     feedback_improve_page_chain = LLMChain(llm=chat, prompt=PromptTemplate.from_template(FEEDBACK_IMPROVE_PAGE_TEMPLATE))
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,14 @@
 import os
 
+LANGCHAIN_BASE = "https://python.langchain.com/docs"
+
 def get_all_paths(directory):
     paths = []
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(".mdx"):
                 paths.append(
-                    "https://python.langchain.com/docs"
+                    LANGCHAIN_BASE
                     + (
                         os.path.join(root, file)
                         .replace(directory, "")
