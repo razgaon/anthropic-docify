053cc603ba14af48d6ccebaf3ee37f40d16d3bf8
Raz Gaon <razgaon@mit.edu>
2023-07-29 16:23:10 -0700

    Changed agents workflow

--- src/agent.py
+++ src/agent.py
@@ -5,24 +5,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from templates import FEEDBACK_IMPROVE_PAGE_TEMPLATE
-from templates_v2 import IMPROVE_PAGE_TEMPLATE_V2, CRITIQUE_PAGE_TEMPLATE_V2
+from templates import FEEDBACK_IMPROVE_PAGE_TEMPLATE, CRITIQUE_PAGE_TEMPLATE, IMPROVE_PAGE_TEMPLATE, INITIAL_CRITIQUE_PAGE_TEMPLATE
 chat = ChatAnthropic(model='claude-2')
 
 def get_improved_page(reference_page: str, context: str, reference_page_name: str) -> str:
-    # Step 1: Given context and a reference page, generate an improved page
-    print(f'Generating improved page for {reference_page_name}')
-    improve_page_chain = LLMChain(llm=chat, prompt=PromptTemplate.from_template(IMPROVE_PAGE_TEMPLATE_V2))
-    improved_page = improve_page_chain.run(context=context, reference_page=reference_page)
-    
+    # Step 1: Give initial critique
+    print(f'Generating initial critique for {reference_page_name}')
+    critique_page_chain = LLMChain(llm=chat, prompt=PromptTemplate.from_template(INITIAL_CRITIQUE_PAGE_TEMPLATE))
+    initial_critique = critique_page_chain.run(context=context, reference_page=reference_page)
     with(open(f'./output/v1/{reference_page_name}.md', 'w')) as f:
+        f.write(critique)
+        
+    # Step 2: Given context and a reference page, generate an improved page
+    print(f'Generating improved page for {reference_page_name}')
+    improve_page_chain = LLMChain(llm=chat, prompt=PromptTemplate.from_template(IMPROVE_PAGE_TEMPLATE))
+    improved_page = improve_page_chain.run(context=context, reference_page=reference_page, critique=critique)
+    
+    with(open(f'./output/v2/{reference_page_name}.md', 'w')) as f:
         f.write(improved_page)
     
     # Step 2: Given the improved page, critique it and provide feedback
     print(f'Generating critique for {reference_page_name}')
-    critique_page_chain = LLMChain(llm=chat, prompt=PromptTemplate.from_template(CRITIQUE_PAGE_TEMPLATE_V2))
+    critique_page_chain = LLMChain(llm=chat, prompt=PromptTemplate.from_template(CRITIQUE_PAGE_TEMPLATE))
     critique = critique_page_chain.run(context=context, reference_page=reference_page, improved_page=improved_page)
-    with(open(f'./output/v2/{reference_page_name}.md', 'w')) as f:
+    with(open(f'./output/v3/{reference_page_name}.md', 'w')) as f:
         f.write(critique)
 
     # Step 3: Given an improved page and critique, generate a new improved page
