e3dbcbc2b1c6c88d9443cf565a7f6df7faba2b3e
Raz Gaon <razgaon@mit.edu>
2023-07-29 18:16:45 -0700

    Improved retrieval

--- src/agent.py
+++ src/agent.py
@@ -6,17 +6,10 @@ from utils import save_output
 
 load_dotenv()
 
-from templates import INITIAL_CRITIQUE_PAGE_TEMPLATE
+from templates import INITIAL_CRITIQUE_PAGE_TEMPLATE, IMPROVE_PAGE_TEMPLATE
 from templates_v2 import IMPROVE_PAGE_TEMPLATE_V2, CRITIQUE_PAGE_TEMPLATE_V2
 
 chat = ChatAnthropic(model='claude-2', temperature=0, max_tokens_to_sample=2048)
-
-"""
-TODO:
-
-- Add a few more rounds that improve the documentation with different but relevant contexts
-
-"""
 
 def get_improved_page(reference_page: str, context: str, reference_page_name: str, n=1) -> str:
     # Step 1: Give initial critique
@@ -28,8 +21,8 @@ def get_improved_page(reference_page: str, context: str, reference_page_name: st
     for i in range(1, n+1):            
         # Step 1: Given context and a reference page, generate an improved page
         print(f'Round {i}: Generating improved page for {reference_page_name}')
-        improve_page_chain = LLMChain(llm=chat, prompt=PromptTemplate.from_template(IMPROVE_PAGE_TEMPLATE_V2))
-        improved_page = improve_page_chain.run(context=context, reference_page=reference_page, critique=initial_critique)
+        improve_page_chain = LLMChain(llm=chat, prompt=PromptTemplate.from_template(IMPROVE_PAGE_TEMPLATE))
+        improved_page = improve_page_chain.run(context=context, reference_page=reference_page, critique=critique)
         save_output(f'./output/improvement/v{i}/{reference_page_name}.md', improved_page)
     
         # Step 2: Given the improved page, critique it and provide feedback
